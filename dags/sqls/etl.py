5db6de029363c323fe8d12e4147735b5423c37e1
Shrikant0504 <patil.shrikant@kisankonnect.in>
2025-09-17 06:11:40 +0000

    connection of databse and write the first script

---
+++ dags/sqls/etl.py
@@ -0,0 +1,7 @@
+insert_customers ='''INSERT INTO users_customer (first_name, last_name, age)
+VALUES
+    ('Alice', 'Johnson', 22),
+    ('Bob', 'Smith', 32),
+    ('Charlie', 'Brown', 44),
+    ('Diana', 'Miller', 33),
+    ('Ethan', 'Williams', 23);'''
